e4183aedeffaef1868123e861237c257912746e5
amdkits <kits.amd@gmail.com>
2026-07-03 02:20:47 +0530

    formatted

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 export default defineConfig({
-  site: 'https://amdkits.me',
-  prefetch: {
-    prefetchAll: true,
-  },
+  site: "https://amdkits.me",
   integrations: [sitemap()],
 });
